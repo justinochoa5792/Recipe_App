aef2b15ab57dbca49e2aa95b093ab52dbec20b8a
Justin Ochoa <just.och92@gmail.com>
2020-09-12 19:23:15 -0400

    first commit no styling

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    userSearch: "",
+    recipe: [],
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    Axios.get(
+      `https://api.edamam.com/search?q=${this.state.userSearch}&app_id=c2f8a963&app_key=50a9c5d1f171508f80243bee7cd4734c`
+    ).then((response) => {
+      console.log(response);
+      this.setState({ recipe: response.data.hits });
+    });
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState(
+      {
+        userSearch: e.target.value,
+      },
+      this.renderRecipes
+    );
+  }
+  renderRecipes() {
+    return this.state.recipe.map((eachRecipe) => {
+      return (
+        <ul>
+          <img src={eachRecipe.recipe.image} />
+          <li>{eachRecipe.recipe.label}</li>
+          <li>{eachRecipe.recipe.calories}</li>
+          <li>{eachRecipe.recipe.source}</li>
+          <a href={eachRecipe.recipe.url}>{eachRecipe.recipe.url}</a>
+        </ul>
+      );
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h2>Search for Recipes</h2>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            placeholder="Find Recipe"
+            onChange={(e) => this.handleChange(e)}
+          />
+        </form>
+        {this.renderRecipes()}
+      </div>
+    );
+  }
+}
+
+export default App;
